75b84210dc46812f7545be108fd61e68eabc233c
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-03 13:07:39 +0100

    First commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,24 @@
+// Addition
+
+let additionOperator = 1 + 1;
+console.log("Addition: " + additionOperator);
+
+// Subtraktion
+
+let subtraktionOperator = 2 - 1;
+console.log("Subtraktion: " + subtraktionOperator);
+
+// Multiplikation
+
+let multiplikationOperator = 2 * 2;
+console.log("Multiplication: " + multiplikationOperator);
+
+// Division
+
+let divisionOperator = 4 / 2;
+console.log("Division: " + divisionOperator);
+
+// Modul: zeigt an, was der Rest sein würde
+
+let modulusOperator = 14 % 5;
+console.log("Modulus: " + modulusOperator);
